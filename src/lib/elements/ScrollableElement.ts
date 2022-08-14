648581f26b8993aeb87bcf68a74b1bfd44db0771
linasjonas <lijonas.it@gmail.com>
2022-08-14 14:38:49 +0300

    Code cleanup

--- src/lib/elements/ScrollableElement.ts
+++ src/lib/elements/ScrollableElement.ts
@@ -50,16 +50,11 @@ export class ScrollableElement {
 
   cloneElements(fitCount: number) {
     const list = this.innerList;
-    const maxScroll = this.scrollWidth - this.width;
-    const allItemsFit = this.items.length <= fitCount;
-    const haventReachedEnd = this.scrollPosition + this.stepSize < maxScroll;
-    // console.log('tried?');
-
-    if (!list || !fitCount || (haventReachedEnd && !allItemsFit)) return;
+    const endReached = this.scrollPosition + this.stepSize * 2 > this.scrollWidth;
+    if (!list || !fitCount || (!endReached && list.children.length > fitCount)) return;
     const prevCurrent = parseInt(list?.dataset.current!, 10);
     for (let i = 0; i < fitCount; i++) {
       const current = (prevCurrent + i) % this._count;
-      // console.log(this._ogItems[current].innerText);
       list.appendChild(this._ogItems[current].cloneNode(true));
       list.dataset.current = ((current + 1) % this._count).toString();
     }
@@ -70,22 +65,7 @@ export class ScrollableElement {
       list.style.setProperty('transition', 'none');
       this.scrollBack();
       setTimeout(() => list.style.removeProperty('transition'), 0);
-      // list.style.removeProperty('transition');
-      // list.classList.remove('notransition');
     }
-    console.log(this.innerList?.children.length);
-
-    // this._scrollHorizontal(this.scrollPosition - this.stepSize - this._itemsMargin * 3);
-
-    // const prevCurrent = parseInt(list?.dataset.current!, 10);
-    // const current = prevCurrent >= this._count ? 0 : prevCurrent;
-    // list.appendChild(this._ogItems[current].cloneNode(true));
-    // if (current > fitCount && this.items.length > this._count) {
-    // this._scrollHorizontal(this.scrollPosition - this.stepSize, 'smooth');
-    // for (let i = 0; i < fitCount; i++) this.items[i].remove();
-    // }
-    // console.log(this.items[0].innerText, this.items.length);
-    // list.dataset.current = (current + 1).toString();
   }
 
   scrollBack() {
