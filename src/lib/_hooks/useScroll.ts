a194dc5cbf02171f0dde0a7dd899fa571c84705e
linasjonas <lijonas.it@gmail.com>
2022-10-23 16:17:22 +0300

    Make bidirectional swap

--- src/lib/_hooks/useScroll.ts
+++ src/lib/_hooks/useScroll.ts
@@ -2,42 +2,30 @@ import { useMemo, useReducer } from 'react';
 
 const DEFAULT_TIME = 600;
 
-type State = { status: 'startReached' | 'scrolled' | 'endReached'; positionsShifted: number; order: number[] };
+type State = { shift: number; order: number[] };
 type Action = 'forward' | 'back';
 
-const initialState: State = { status: 'startReached', positionsShifted: 0, order: [] };
+const initialState: State = { shift: 0, order: [] };
 
 const reducer =
   (itemsPerDisplay: number, items: Element[], listEl: HTMLDivElement | null, stepSize: number) =>
   (state: State, action: Action) => {
     const defaultOrder = items.map((_, index) => index + 1);
+    const deficiency = items.length % itemsPerDisplay;
     const order = state.order.length ? state.order : defaultOrder;
 
     switch (action) {
       case 'forward': {
-        if (state.status === 'endReached') return state;
-        const endWillBeReached = state.positionsShifted + itemsPerDisplay * 2 > items.length;
-        items.forEach((el, index) => updateElementOrder(order[index])(el as any));
-        updateListPosition(listEl, state.positionsShifted + stepSize);
-
-        return {
-          status: endWillBeReached ? 'endReached' : 'scrolled',
-          positionsShifted: state.positionsShifted + stepSize,
-          order: order.map(i =>
-            i + itemsPerDisplay > items.length ? i + itemsPerDisplay - items.length : i + itemsPerDisplay,
-          ),
-        };
-      }
-      case 'back': {
-        const endWillBeReached = state.positionsShifted - stepSize * 2 < -(listEl?.scrollWidth ?? 0);
+        const endWillBeReached =
+          state.shift - stepSize - (stepSize / itemsPerDisplay) * deficiency < -(listEl?.scrollWidth ?? 0);
 
         if (endWillBeReached) {
-          const nextShift = state.positionsShifted - (items.length % itemsPerDisplay) * (stepSize / itemsPerDisplay);
+          const nextShift = state.shift - (stepSize / itemsPerDisplay) * deficiency;
           updateListPosition(listEl, nextShift);
 
           const newOrder = [
-            ...order.slice(-itemsPerDisplay * 2 + 1),
-            ...order.slice(0, items.length - itemsPerDisplay * 2 + 1),
+            ...order.slice(-itemsPerDisplay * 2 + (itemsPerDisplay - deficiency)),
+            ...order.slice(0, -itemsPerDisplay * 2 + (itemsPerDisplay - deficiency)),
           ];
 
           setTimeout(() => {
@@ -47,24 +35,44 @@ const reducer =
             setTimeout(() => updateListAnimationTime(DEFAULT_TIME)(listEl), DEFAULT_TIME);
           }, DEFAULT_TIME);
 
-          // updateListAnimationTime(DEFAULT_TIME)(listEl);
+          return { shift: 0, order: newOrder };
+        }
+
+        const nextShift = state.shift - stepSize;
+        updateListPosition(listEl, nextShift);
+
+        return { shift: nextShift, order };
+      }
+      case 'back': {
+        const startWillBeReached = state.shift === 0;
+
+        if (startWillBeReached) {
+          const newOrder = [
+            ...order.slice(-itemsPerDisplay * 2 + (itemsPerDisplay - deficiency)),
+            ...order.slice(0, -itemsPerDisplay * 2 + (itemsPerDisplay - deficiency)),
+          ];
+
+          items.forEach((el, index) => updateElementOrder(newOrder[index])(el as any));
+          updateListAnimationTime(0)(listEl);
+          updateListPosition(listEl, -stepSize);
+
+          setTimeout(() => {
+            updateListAnimationTime(DEFAULT_TIME)(listEl);
+            updateListPosition(listEl, 0);
+          }, 0);
 
           return {
-            status: endWillBeReached ? 'startReached' : 'scrolled',
-            positionsShifted: 0,
-            order: newOrder,
+            shift: 0,
+            order: order.map(i =>
+              i + itemsPerDisplay > items.length ? i + itemsPerDisplay - items.length : i + itemsPerDisplay,
+            ),
           };
         }
 
-        const nextShift = state.positionsShifted - stepSize;
-        // items.forEach((el, index) => updateElementOrder(order[index])(el as any));
+        const nextShift = state.shift + stepSize;
         updateListPosition(listEl, nextShift);
 
-        return {
-          status: endWillBeReached ? 'scrolled' : 'scrolled',
-          positionsShifted: nextShift,
-          order,
-        };
+        return { shift: nextShift, order };
       }
       default:
         throw new Error();
@@ -72,11 +80,8 @@ const reducer =
   };
 
 export const useScroll = (listEl: HTMLDivElement | null, itemsPerDisplay: number, stepSize: number) => {
-  // const itemWidth = stepSize / itemsPerDisplay;
   const items = useMemo(() => (listEl && Array.from(ulElement(listEl).children)) || [], [listEl]);
   const [_state, dispatch] = useReducer(reducer(itemsPerDisplay, items, listEl, stepSize), initialState);
-
-  // useEffect(() => updateListAnimationTime(DEFAULT_TIME)(listEl), [listEl]);
 
   return {
     forward: () => dispatch('forward'),
