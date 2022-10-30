d23546b1a5ca8b3d027d940b3d62fc51f926c456
linasjonas <lijonas.it@gmail.com>
2022-10-30 03:23:28 +0300

    cleanup

--- src/lib/_hooks/reducer.ts
+++ src/lib/_hooks/reducer.ts
@@ -23,40 +23,37 @@ const reducer =
     const itemsCount = countListItems(listEl);
     const listWidth = listEl?.scrollWidth ?? 0;
     const stepSize = listEl?.clientWidth ?? 0;
-    const emptySpacedStepSize = (stepSize / shownItemsCount) * (itemsCount % shownItemsCount);
+    const overStepSize = (stepSize / shownItemsCount) * (itemsCount % shownItemsCount);
 
     switch (action) {
       case 'forward': {
-        const endReached = state.shift - stepSize <= -listWidth;
-        const endWillBeReached = state.shift - stepSize - emptySpacedStepSize < -listWidth;
+        const endReached = state.shift + stepSize >= listWidth;
+        const endWillBeReached = state.shift + stepSize + overStepSize > listWidth;
 
         if (type === 'finite' && endReached) {
           return state;
         }
 
         if (type === 'finite' && endWillBeReached) {
-          const nextShift = state.shift - emptySpacedStepSize;
+          const nextShift = state.shift + overStepSize;
           slideTo(listEl, { shift: nextShift });
           return { ...state, shift: nextShift };
         }
 
         if (type === 'infinite' && endWillBeReached) {
-          const nextShift = state.shift - emptySpacedStepSize;
+          const nextShift = state.shift + overStepSize;
           slideTo(listEl, { shift: nextShift });
-          delay(() => {
-            slideTo(listEl, { shift: 0, time: 0, callback: () => order.reorder() });
-            delay(() => slideTo(listEl));
-          });
+          delay(() => slideTo(listEl, { shift: 0, time: 0, callback: () => order.reorder() }));
           return { ...state, shift: 0 };
         }
 
-        const nextShift = state.shift - stepSize;
+        const nextShift = state.shift + stepSize;
         slideTo(listEl, { shift: nextShift });
-        return { ...state, shift: state.shift - stepSize };
+        return { ...state, shift: nextShift };
       }
       case 'back': {
-        const startReached = state.shift >= 0;
-        const startWillBeReached = state.shift + stepSize >= 0;
+        const startReached = state.shift <= 0;
+        const startWillBeReached = state.shift - stepSize <= 0;
 
         if (type === 'finite' && startReached) {
           return state;
@@ -69,12 +66,13 @@ const reducer =
         }
 
         if (type === 'infinite' && startReached) {
-          slideTo(listEl, { shift: -stepSize, time: 0, callback: () => order.reorder() });
+          const nextShift = stepSize;
+          slideTo(listEl, { shift: nextShift, time: 0, callback: () => order.reorder() });
           delayTillNextFrame(() => slideTo(listEl, { shift: 0 }));
           return { ...state, shift: 0 };
         }
 
-        const nextShift = state.shift + stepSize;
+        const nextShift = state.shift - stepSize;
         slideTo(listEl, { shift: nextShift });
         return { ...state, shift: nextShift };
       }
